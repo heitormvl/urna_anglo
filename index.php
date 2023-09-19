89a03a9fe0dce0650adf3464519cd99f31a3b9f3
Heitor Maciel <heitormvl12@gmail.com>
2023-09-18 22:42:19 -0300

    Cria projeto

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Hello World</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <h1 class="text-center">Hello World</h1>
+        </div>
+    </div>
+</body>
+</html>
