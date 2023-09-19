70a55c6bd5151c4ee9b4745de411a225ab8d8f67
heitormvl <heitormvl12@gmail.com>
2023-09-19 01:37:45 -0300

    Front-end

--- index.php
+++ index.php
@@ -1,15 +1,56 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <title>PHP Hello World</title>
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-    <div class="container">
-        <div class="row">
-            <h1 class="text-center">Hello World</h1>
+    <div class="container-fluid">
+        <div class="tela">
+            <div class="row">
+                <div class="col-8">
+                    <h2 class="cargo">
+                        Representante de Turma
+                    </h2>
+                    <form action="confirmar_voto.php" method="POST" class="pt-3">
+                        <input type="number" name="digito1" id="digito1">
+                        <input type="number" name="digito2" id="digito2">
+                        <div class="dados_candidato">
+                            <div class="dados">
+                                <span>
+                                    Nome:
+                                </span>
+                                <span class="dado" id="nome_candidato">Fulano</span>
+                            </div>
+                            <div class="dados">
+                                <span>
+                                    Partido:
+                                </span>
+                                <span class="dado" id="partido_candidato">Partido X - PX</span>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div class="col-4">
+                    <div class="foto-candidato"></div>
+                </div>
+            </div>
+            <div class="row rodape mt-4 p-1">
+                <div class="col-12">
+                    <div>
+                        PRESSIONE A TECLA <code>ENTER</code> PARA CONFIRMAR ESTE VOTO
+                    </div>
+                    <div>
+                        PRESSIONE A TECLA <code>BACKSPACE</code> PARA CORRIGIR ESTE VOTO
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </body>
+
 </html>
